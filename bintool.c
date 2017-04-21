ee267ceeeb14ae1b0bc2c423b2c9a7db3bb2f738
Ananth Mudumba <m.ananth7592@gmail.com>
2017-04-21 15:45:19 -0700

    Second commit

--- bintool.c
+++ bintool.c
@@ -1,25 +1,23 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <dlfcn.h>
 
-void *malloc(int size);
-void free(void *ptr);
-
 void *malloc(int req_size)
 {
-	static *(void **)(acutal_malloc)(int size);
+	void*(*actual_malloc)(int size);
 	void *m_ptr;
 
-	acutal_malloc = dlsym(RTLD_NEXT, "malloc");
-	m_ptr = acutal_malloc(req_size);
+	actual_malloc = dlsym(RTLD_NEXT, "malloc");
+	m_ptr = actual_malloc(req_size);
 	return m_ptr;
 
 }
 
 void free(void *req_ptr)
 {
-	static *(void **)(acutal_free)(void *ptr);
+	void *(*actual_free)(void *ptr);
 
-	acutal_free = dlsym(RTLD_NEXT, "free");
+	actual_free = dlsym(RTLD_NEXT, "free");
 	free(req_ptr);
 
 }
