423056085fcc560d5faae7cf3d45244a826aeb9f
Ananth Mudumba <m.ananth7592@gmail.com>
2017-04-21 16:40:25 -0700

    Malloc works. Helper functions

--- bintool.c
+++ bintool.c
@@ -2,12 +2,38 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
+typedef struct {
+	void *mem_ptr;
+	size_t size;
+	char[6] canary;
+}mem_track;
+
+mem_track ins[999];
+
+
+/*Returns -1 if memory access is denied
+           size of the Memory chunk otherwise */
+size_t check_memory_access(void *  ptr)
+{
+
+}
+
+void   del_memory_track(void *ptr)
+{
+
+}
+
+void update_memory_track(void *ptr, size_t size)
+{
+	
+}
 void *malloc(int req_size)
 {
 	void*(*actual_malloc)(int size);
 	void *m_ptr;
 
 	actual_malloc = dlsym(RTLD_NEXT, "malloc");
+        fprintf(stdout, "hello\n");
 	m_ptr = actual_malloc(req_size);
 	return m_ptr;
 
@@ -18,6 +44,6 @@ void free(void *req_ptr)
 	void *(*actual_free)(void *ptr);
 
 	actual_free = dlsym(RTLD_NEXT, "free");
-	free(req_ptr);
+	actual_free(req_ptr);
 
 }
