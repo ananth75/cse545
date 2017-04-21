6aa336d7f8d6e9007c5ebb8183b09625eb156570
Ananth M <m.ananth7592@gmail.com>
2017-04-21 15:24:35 -0700

    Check dlsym functionality

--- bintool.c
+++ bintool.c
@@ -2,3 +2,24 @@
 #include <dlfcn.h>
 
 void *malloc(int size);
+void free(void *ptr);
+
+void *malloc(int req_size)
+{
+	static *(void **)(acutal_malloc)(int size);
+	void *m_ptr;
+
+	acutal_malloc = dlsym(RTLD_NEXT, "malloc");
+	m_ptr = acutal_malloc(req_size);
+	return m_ptr;
+
+}
+
+void free(void *req_ptr)
+{
+	static *(void **)(acutal_free)(void *ptr);
+
+	acutal_free = dlsym(RTLD_NEXT, "free");
+	free(req_ptr);
+
+}
